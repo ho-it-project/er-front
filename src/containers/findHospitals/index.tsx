832f550c251bd7741bfa20407d4b4701d3068895
ejinn1 <ejinn1@naver.com>
2023-10-09 09:48:38 +0900

    feat: findHospital_page animation 추가

--- src/containers/findHospitals/index.tsx
+++ src/containers/findHospitals/index.tsx
@@ -2,9 +2,12 @@
 
 import TopNavContentWrapper from "@/components/TopNavContentWrapper";
 import SearchInput from "@/components/common/SearchInput";
-import { useEffect, useState } from "react";
+import usePageTransition from "@/states/pageTransitionStore";
+import { Transition } from "@headlessui/react";
+import { ReactNode, useEffect, useState } from "react";
 import ContentWrapper from "../../components/common/ContentWrapper";
 import PlaceBox from "../../components/common/PlaceBox";
+import MessageBox from "../message/messageBox";
 import HospitalInfo from "./hospitalInfo";
 import Nav from "./nav";
 import PlaceDetailBox from "./placeDetailBox";
@@ -17,14 +20,17 @@ interface EmergencyCenter {
   emergency_center_address: string;
 }
 
-const TopNavs = [{ title: "타병원 찾기", link: "/findHospitals" }];
+const TopNavs1 = [{ title: "타병원 찾기", link: "/findHospitals" }];
+const TopNavs2 = [{ title: "메세지", link: "/message" }];
 
 export default function FindHospitalsContainer() {
+  const { isPageTransitioning, startPageTransition } = usePageTransition();
+
   const [selectedHospital, setSelectedHospital] = useState<EmergencyCenter>();
   const [, setSearchWord] = useState("");
   const [clickedNav, setClickedNav] = useState("전체");
   const [emergency, setEmergency] = useState<EmergencyCenter[]>();
-  const [isLoading, setIsLoading] = useState(true);
+  const [, setIsLoading] = useState(true);
 
   useEffect(() => {
     const { geolocation } = navigator;
@@ -66,10 +72,17 @@ export default function FindHospitalsContainer() {
     }
   };
 
+  // 버튼 클릭시 message로 전환 애니메이션
+  const ClickedChagneMessage = () => {
+    startPageTransition();
+  };
+
   return (
     <>
-      <TopNavContentWrapper topNav={{ items: TopNavs }}>
-        <div>
+      <TopNavContentWrapper
+        topNav={isPageTransitioning ? { items: TopNavs2 } : { items: TopNavs1 }}
+      >
+        <MainContent isPageTransitioning={!isPageTransitioning}>
           <div className="sticky top-0 z-[1] mb-[5rem] mr-[4rem] flex h-[7rem] w-full items-center justify-between bg-white py-[1rem] pr-[4rem]">
             <Nav onClickNav={ClickedNavHandler} />
             <SearchInput
@@ -79,25 +92,13 @@ export default function FindHospitalsContainer() {
               }}
             />
           </div>
-          {isLoading ? (
-            <div>로딩중...</div>
-          ) : (
-            <>
-              {emergency !== undefined && (
-                <div className="flex w-full flex-col gap-[2rem]">
-                  {emergency.map((e, index) => (
+          {emergency !== undefined && (
+            <div className="flex w-full flex-col gap-[2rem]">
+              {emergency.map((e, index) => (
+                <>
+                  {clickedNav !== "전체" ? (
                     <>
-                      {clickedNav !== "전체" ? (
-                        <>
-                          {clickedNav == e.emergency_center_type && (
-                            <EmergencyBox
-                              index={index}
-                              ChangeSelectedHospital={ChangeSelectedHospital}
-                              e={e}
-                            />
-                          )}
-                        </>
-                      ) : (
+                      {clickedNav == e.emergency_center_type && (
                         <EmergencyBox
                           index={index}
                           ChangeSelectedHospital={ChangeSelectedHospital}
@@ -105,36 +106,113 @@ export default function FindHospitalsContainer() {
                         />
                       )}
                     </>
-                  ))}
-                </div>
-              )}
-            </>
+                  ) : (
+                    <EmergencyBox
+                      index={index}
+                      ChangeSelectedHospital={ChangeSelectedHospital}
+                      e={e}
+                    />
+                  )}
+                </>
+              ))}
+            </div>
           )}
-        </div>
-      </TopNavContentWrapper>
-      {selectedHospital && (
-        <div className="mt-[7rem] flex h-[calc(100%-8rem)] w-full min-w-[32rem] flex-col">
-          <PlaceBox place={selectedHospital.emergency_center_name} />
-          <div className="flex h-[calc(100%-10rem)] w-full justify-between gap-[2rem]">
-            <ContentWrapper>
-              <PlaceDetailBox
-                sub={`${
-                  selectedHospital.emergency_center_type ==
-                  "REGIONAL_EMERGENCY_MEDICAL_CENTER"
-                    ? "지역응급의료센터"
-                    : selectedHospital.emergency_center_type ==
-                      "NON_EMERGENCY_MEDICAL_INSTITUTION"
-                    ? "지역응급의료기관"
-                    : "권역응급의료센터"
-                }`}
-                number={selectedHospital.emergency_center_primary_phone}
-                location={selectedHospital.emergency_center_address}
-              />
-            </ContentWrapper>
+        </MainContent>
+        <MainContent isPageTransitioning={isPageTransitioning}>
+          <div className="h-full w-full min-w-[32rem]">
+            <div className="flex w-full justify-between text-[1.5rem] text-L-gray">
+              <div>대화창</div>
+              <div className="text-[3rem]">+</div>
+            </div>
+            <div className="flex h-full flex-col gap-[1.5rem] px-[1.5rem]">
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+              <MessageBox />
+            </div>
           </div>
-        </div>
-      )}
+        </MainContent>
+      </TopNavContentWrapper>
+
+      <Transition
+        className={`w-full`}
+        show={selectedHospital !== undefined}
+        enter="transition-all duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-all duration-300"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        {(ref) => (
+          <div
+            ref={ref}
+            className={`mt-[7rem] flex h-[calc(100%-7rem)] w-full flex-col`}
+          >
+            {selectedHospital && (
+              <>
+                <PlaceBox
+                  place={selectedHospital.emergency_center_name}
+                  ClickedChagneMessage={ClickedChagneMessage}
+                />
+                <div className="flex h-[calc(100%-11rem)] w-full gap-[2rem]">
+                  <ChatingBox isPageTransitioning={isPageTransitioning} />
+                  <div className="flex h-full w-full min-w-[32rem] justify-between gap-[2rem] transition-all duration-300">
+                    <ContentWrapper>
+                      <PlaceDetailBox
+                        sub={`${
+                          selectedHospital.emergency_center_type ==
+                          "REGIONAL_EMERGENCY_MEDICAL_CENTER"
+                            ? "지역응급의료센터"
+                            : selectedHospital.emergency_center_type ==
+                              "NON_EMERGENCY_MEDICAL_INSTITUTION"
+                            ? "지역응급의료기관"
+                            : "권역응급의료센터"
+                        }`}
+                        number={selectedHospital.emergency_center_primary_phone}
+                        location={selectedHospital.emergency_center_address}
+                      />
+                    </ContentWrapper>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        )}
+      </Transition>
     </>
+  );
+}
+
+interface MainContentProps {
+  children: ReactNode;
+  isPageTransitioning: boolean;
+}
+
+function MainContent({ children, isPageTransitioning }: MainContentProps) {
+  return (
+    <Transition
+      className="h-full w-full"
+      show={isPageTransitioning}
+      enter="transition-opacity opacity-0 duration-300" // 나타날 때 opacity 0에서 1로 변경
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity opacity-100 duration-0" // 사라질 때 opacity 1에서 0으로 변경
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {children}
+    </Transition>
   );
 }
 
@@ -168,5 +246,38 @@ function EmergencyBox({ index, ChangeSelectedHospital, e }: EmergencyBoxProps) {
         location={e.emergency_center_address}
       />
     </div>
+  );
+}
+
+interface ChatingBoxProps {
+  isPageTransitioning: boolean;
+}
+
+function ChatingBox({ isPageTransitioning }: ChatingBoxProps) {
+  return (
+    <Transition
+      show={isPageTransitioning}
+      enter="transition-opacity duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave=""
+      leaveFrom=""
+      leaveTo="opacity-0"
+    >
+      {(ref) => (
+        <div
+          ref={ref}
+          className="flex h-full w-full min-w-[62rem] justify-between gap-[2rem]"
+        >
+          <ContentWrapper>
+            <div className="absolute bottom-[2rem] h-[5.5rem] w-[56rem] rounded-full border-2 border-main bg-white p-[1rem] px-[2rem]">
+              <div className="flex h-full flex-col justify-center">
+                <input className="w-full bg-white text-[1.5rem] focus:outline-none" />
+              </div>
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </Transition>
   );
 }
