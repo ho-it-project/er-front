6c6983836d49ebebba3acd5cb60fb18e273a715b
ejinn1 <ejinn1@naver.com>
2023-11-11 16:34:39 +0900

    feat: 치료가능질환 api연동 (수정필요함)

--- src/containers/severeEmergencyillness/index.tsx
+++ src/containers/severeEmergencyillness/index.tsx
@@ -1,102 +1,91 @@
+"use client";
+
+import useUpdateServableListStore from "@/states/updateServableIllnessList";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 import SevereBox from "./severeBox";
 import SevereButton from "./severeButton";
 
-const SEVERE_DUMMY = [
-  {
-    title: "재관류중재술",
-    sub: [
-      { disease: "심근경색", state: true },
-      { disease: "뇌경색", state: false },
-    ],
-  },
-  {
-    title: "뇌출혈수술",
-    sub: [
-      { disease: "거미막하출혈", state: true },
-      { disease: "거미막하출혈 외", state: false },
-    ],
-  },
-  {
-    title: "대동맥응급",
-    sub: [
-      { disease: "흉부", state: true },
-      { disease: "복부", state: false },
-    ],
-  },
-  {
-    title: "사지접합",
-    sub: [
-      { disease: "수족지접합", state: true },
-      { disease: "수족지접합 외", state: false },
-    ],
-  },
-  {
-    title: "담낭담관질환",
-    sub: [
-      { disease: "담낭질환", state: true },
-      { disease: "담도포함질환", state: false },
-    ],
-  },
-  {
-    title: "응급투석",
-    sub: [
-      { disease: "HD", state: true },
-      { disease: "CRRT", state: true },
-    ],
-  },
-  {
-    title: "응급내시경",
-    sub: [
-      { disease: "성인 위장관", state: true },
-      { disease: "영유아 위장관", state: false },
-      { disease: "성인 기관지", state: false },
-      { disease: "영유아 기관지", state: false },
-    ],
-  },
-  {
-    title: "복부응급수술",
-    sub: [{ disease: "비외상", state: true }],
-  },
-  {
-    title: "장중첩/폐색",
-    sub: [{ disease: "영유아", state: true }],
-  },
-  {
-    title: "영상의학혈관중재",
-    sub: [
-      { disease: "성인", state: true },
-      { disease: "영유아", state: true },
-    ],
-  },
+interface severeGroup {
+  servable_title: string;
+  servable_illness: severeIllness[];
+}
 
-  {
-    title: "산부인과응급",
-    sub: [
-      { disease: "분만", state: true },
-      { disease: "산과수술", state: false },
-      { disease: "부인과수술", state: false },
-    ],
-  },
-  {
-    title: "저출생체중아",
-    sub: [{ disease: "집중치료", state: true }],
-  },
-
-  {
-    title: "정신과적응급",
-    sub: [{ disease: "폐쇄병동입원", state: true }],
-  },
-  {
-    title: "중증화상",
-    sub: [{ disease: "전문치료", state: true }],
-  },
-  {
-    title: "인과적수술",
-    sub: [{ disease: "응급", state: true }],
-  },
-];
+interface severeIllness {
+  servable_illness_id: string;
+  servable_illness_name: string;
+  status: "ACTIVE" | "INACTIVE";
+}
 
 export default function SevereEmergencyIllnessContainer() {
+  const url = "/api/er/hospitals/current/illness";
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+  const { data, isLoading } = useSWR(url, fetcher);
+  const [severeConditions, setSevereConditions] = useState<severeGroup[]>();
+
+  const { updateList, addUpdateList } = useUpdateServableListStore();
+
+  const clickHandler = (id: string, status: "ACTIVE" | "INACTIVE") => () => {
+    addUpdateList(id, status);
+    console.log(updateList);
+  };
+
+  const deparmentUpdateSubmit = () => {
+    const url = "/api/er/hospitals/current/illness";
+    console.log(updateList);
+
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateList),
+    })
+      .then((r) => r.json())
+      .then((d) => console.log(d));
+  };
+
+  useEffect(() => {
+    if (data && data.result) {
+      const groupedData = data.result
+        .slice(0, 29)
+        .reduce((acc: severeGroup[], item: severeIllness) => {
+          const match = item.servable_illness_name.match(/\[(.*?)](.*)/);
+          const servableTitle = match && match[1];
+          const servableName = match && match[2].trim();
+
+          if (!servableName || !servableTitle) return;
+
+          const existingItem = acc.find(
+            (group) =>
+              servableTitle && group.servable_title.includes(servableTitle)
+          );
+
+          if (existingItem) {
+            existingItem.servable_illness.push({
+              servable_illness_id: item.servable_illness_id,
+              servable_illness_name: servableName,
+              status: item.status,
+            });
+          } else {
+            acc.push({
+              servable_title: servableTitle,
+              servable_illness: [
+                {
+                  servable_illness_id: item.servable_illness_id,
+                  servable_illness_name: servableName,
+                  status: item.status,
+                },
+              ],
+            });
+          }
+
+          return acc;
+        }, []);
+      setSevereConditions(groupedData);
+    }
+  }, [data]);
+
   return (
     <>
       <div className="px-[6rem] py-[6rem]">
@@ -104,19 +93,32 @@ export default function SevereEmergencyIllnessContainer() {
           <p className="ml-[6rem] w-[24rem] text-[1.2rem] font-[600] text-gray">
             • 현재 진료 가능한 중증응급질환을 선택해주세요.
           </p>
-          <button className="h-[5rem] w-[20rem] rounded-[1rem] bg-main text-[1.6rem] font-[600] text-white">
+          <button
+            className="h-[5rem] w-[20rem] rounded-[1rem] bg-main text-[1.6rem] font-[600] text-white"
+            onClick={deparmentUpdateSubmit}
+          >
             저장하기
           </button>
         </div>
         <div className="mt-[12rem] h-[62rem] w-full">
-          <div className="flex flex-wrap gap-[4rem] ">
-            {SEVERE_DUMMY.map((severe, index) => (
-              <SevereBox title={severe.title} key={index}>
-                {severe.sub.map((sub, index) => (
-                  <SevereButton sub={sub.disease} set={sub.state} key={index} />
-                ))}
-              </SevereBox>
-            ))}
+          <div className="flex flex-wrap gap-[4rem]">
+            {severeConditions &&
+              severeConditions.map((severe, index) => (
+                <SevereBox title={severe.servable_title} key={index}>
+                  {severe.servable_illness.map((illness, index) => (
+                    <SevereButton
+                      name={illness.servable_illness_name}
+                      status={illness.status}
+                      id={illness.servable_illness_id}
+                      onClick={clickHandler(
+                        illness.servable_illness_id,
+                        illness.status === "ACTIVE" ? "INACTIVE" : "ACTIVE"
+                      )}
+                      key={index}
+                    />
+                  ))}
+                </SevereBox>
+              ))}
           </div>
         </div>
       </div>
---
+++ src/states/updateServableIllnessList.tsx
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+
+interface severeIllness {
+  servable_illness_id: string;
+  servable_illness_name: string;
+  status: "ACTIVE" | "INACTIVE";
+}
+
+interface UpdateInfo {
+  illness_id: string;
+  illness_status: "ACTIVE" | "INACTIVE";
+}
+
+interface UpdateListState {
+  updateList: UpdateInfo[];
+  addUpdateList: (id: string, status: "ACTIVE" | "INACTIVE") => void;
+}
+
+const useUpdateServableListStore = create<UpdateListState>((set) => ({
+  updateList: [],
+  addUpdateList: (id, status) => {
+    set((state) => {
+      // 이미 존재하는 ID를 찾음
+      const existingIndex = state.updateList.findIndex(
+        (item) => item.illness_id === id
+      );
+
+      // 만약 ID가 이미 존재한다면 해당 ID의 status를 업데이트
+      if (existingIndex !== -1) {
+        state.updateList[existingIndex].illness_status =
+          state.updateList[existingIndex].illness_status === "ACTIVE"
+            ? "INACTIVE"
+            : "ACTIVE";
+      } else {
+        // ID가 존재하지 않으면 새로운 항목 추가
+        state.updateList.push({ illness_id: id, illness_status: status });
+      }
+
+      return { updateList: state.updateList };
+    });
+  },
+}));
+
+export default useUpdateServableListStore;
