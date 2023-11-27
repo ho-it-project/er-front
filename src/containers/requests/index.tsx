17c9161c5b1d684cf83193f29ed2fb438bdf426d
ejinn1 <ejinn1@naver.com>
2023-11-27 16:48:57 +0900

    fix: 요청 목록에서 요청 누르면 요청상세보기 모달 뜨도록 수정

--- src/containers/requests/index.tsx
+++ src/containers/requests/index.tsx
@@ -2,14 +2,20 @@
 
 import Spinner from "@/components/Spinner";
 import TopNavContentWrapper from "@/components/TopNavContentWrapper";
+import useModal from "@/hooks/useModal";
 import { useRequestList } from "@/hooks/useRequestList";
 import {
   transeformName,
   transformAge,
   transformDate,
 } from "@/lib/utils/transeform";
-import { RequestStatus, useRequestListStore } from "@/states/requestStore";
+import {
+  Request,
+  RequestStatus,
+  useRequestListStore,
+} from "@/states/requestStore";
 import { useState } from "react";
+import RequestDetailModal from "./requestDetailModal";
 import RequestHeader from "./requestHeader";
 import RequestItem from "./requestItem";
 import RequestNav from "./requestNav";
@@ -19,8 +25,14 @@ const TopNavRequest = [{ title: "요청 목록", link: "/requests" }];
 export default function RequestsContainer() {
   const { setQueryStatus } = useRequestListStore();
   const { requests, isLoading } = useRequestList();
+  const { isOpen, openModal, closeModal } = useModal();
 
   console.log("request_page", requests);
+
+  const [selectedRequest, setSelectedRequest] = useState<Request>();
+  const clickRequestHanlder = (request: Request) => {
+    setSelectedRequest(request);
+  };
 
   const [, setClickedNav] = useState<RequestStatus[] | "전체">("전체");
   const ClickedNavHandler = (value: RequestStatus[] | "전체") => {
@@ -46,17 +58,34 @@ export default function RequestsContainer() {
           <RequestHeader />
           <div className="mt-[2rem] h-[calc(100%-8rem)] w-full overflow-scroll">
             {requests.map((request) => (
-              <RequestItem
+              <div
+                className="cursor-pointer"
                 key={request.emergency_center_id + request.patient_id}
-                requestDate={transformDate(request.request_date)}
-                gender={request.patient.patient_gender === "MALE" ? "남" : "여"}
-                age={transformAge(request.patient.patient_birth)}
-                companyName={transeformName(request.emergency_center_name)}
-                symptom={request.patient.patient_symptom_summary}
-                status={request.request_status}
-              />
+                onClick={() => {
+                  clickRequestHanlder(request);
+                  openModal();
+                }}
+              >
+                <RequestItem
+                  requestDate={transformDate(request.request_date)}
+                  gender={
+                    request.patient.patient_gender === "MALE" ? "남" : "여"
+                  }
+                  age={transformAge(request.patient.patient_birth)}
+                  companyName={transeformName(request.emergency_center_name)}
+                  symptom={request.patient.patient_symptom_summary}
+                  status={request.request_status}
+                />
+              </div>
             ))}
           </div>
+          {isOpen && selectedRequest && (
+            <RequestDetailModal
+              request={selectedRequest}
+              patient={selectedRequest.patient}
+              closeModal={closeModal}
+            />
+          )}
         </div>
       )}
     </TopNavContentWrapper>
