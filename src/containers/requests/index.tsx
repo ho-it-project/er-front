802a67fc545144c86e865864d2bd5ca7af668d20
ejinn1 <ejinn1@naver.com>
2023-11-28 16:15:57 +0900

    fix: 요청 리스트 filter로 변경

--- src/containers/requests/index.tsx
+++ src/containers/requests/index.tsx
@@ -9,11 +9,7 @@ import {
   transformAge,
   transformDate,
 } from "@/lib/utils/transeform";
-import {
-  Request,
-  RequestStatus,
-  useRequestListStore,
-} from "@/states/requestStore";
+import { Request, RequestStatus } from "@/states/requestStore";
 import { useState } from "react";
 import RequestDetailModal from "./requestDetailModal";
 import RequestHeader from "./requestHeader";
@@ -23,27 +19,19 @@ import RequestNav from "./requestNav";
 const TopNavRequest = [{ title: "요청 목록", link: "/requests" }];
 
 export default function RequestsContainer() {
-  const { setQueryStatus } = useRequestListStore();
   const { requests, isLoading } = useRequestList();
   const { isOpen, openModal, closeModal } = useModal();
-
-  console.log("request_page", requests);
 
   const [selectedRequest, setSelectedRequest] = useState<Request>();
   const clickRequestHanlder = (request: Request) => {
     setSelectedRequest(request);
   };
 
-  const [, setClickedNav] = useState<RequestStatus[] | "전체">("전체");
+  const [clickedNav, setClickedNav] = useState<RequestStatus[] | "전체">(
+    "전체"
+  );
   const ClickedNavHandler = (value: RequestStatus[] | "전체") => {
     setClickedNav(value);
-    console.log(value);
-
-    if (value !== "전체") {
-      setQueryStatus(value);
-    } else {
-      setQueryStatus([]);
-    }
   };
 
   return (
@@ -57,27 +45,33 @@ export default function RequestsContainer() {
           </div>
           <RequestHeader />
           <div className="mt-[2rem] h-[calc(100%-8rem)] w-full overflow-scroll">
-            {requests.map((request) => (
-              <div
-                className="cursor-pointer"
-                key={request.emergency_center_id + request.patient_id}
-                onClick={() => {
-                  clickRequestHanlder(request);
-                  openModal();
-                }}
-              >
-                <RequestItem
-                  requestDate={transformDate(request.request_date)}
-                  gender={
-                    request.patient.patient_gender === "MALE" ? "남" : "여"
-                  }
-                  age={transformAge(request.patient.patient_birth)}
-                  companyName={transeformName(request.emergency_center_name)}
-                  symptom={request.patient.patient_symptom_summary}
-                  status={request.request_status}
-                />
-              </div>
-            ))}
+            {requests
+              .filter(
+                (request) =>
+                  clickedNav === "전체" ||
+                  clickedNav.includes(request.request_status)
+              )
+              .map((request) => (
+                <div
+                  className="cursor-pointer"
+                  key={request.emergency_center_id + request.patient_id}
+                  onClick={() => {
+                    clickRequestHanlder(request);
+                    openModal();
+                  }}
+                >
+                  <RequestItem
+                    requestDate={transformDate(request.request_date)}
+                    gender={
+                      request.patient.patient_gender === "MALE" ? "남" : "여"
+                    }
+                    age={transformAge(request.patient.patient_birth)}
+                    companyName={transeformName(request.emergency_center_name)}
+                    symptom={request.patient.patient_symptom_summary}
+                    status={request.request_status}
+                  />
+                </div>
+              ))}
           </div>
           {isOpen && selectedRequest && (
             <RequestDetailModal
