5c7581161691bdabcb4d01bfbf5f752ba54581ad
ejinn1 <ejinn1@naver.com>
2023-12-07 22:50:40 +0900

    fix: 타병원 병상상태 표현 수정

--- src/containers/findHospitals/emergencyBox.tsx
+++ src/containers/findHospitals/emergencyBox.tsx
@@ -7,11 +7,31 @@ interface EmergencyBoxProps {
   emergencyCenter: EmergencyCenter;
 }
 
+const getColorByRatio = (occupiedCount: number, totalCount: number) => {
+  const ratio = occupiedCount / totalCount;
+  if (ratio > 0.5) {
+    return "red";
+  } else if (ratio > 0.3) {
+    return "yellow";
+  } else {
+    return "main";
+  }
+};
+
 export default function EmergencyBox({
   index,
   ChangeSelectedHospital,
   emergencyCenter,
 }: EmergencyBoxProps) {
+  const roomInfo = emergencyCenter.emergency_rooms.map((room) => ({
+    name: room.emergency_room_name,
+    color: getColorByRatio(
+      room.emergency_room_beds.filter(
+        (bed) => bed.emergency_room_bed_status === "OCCUPIED"
+      ).length,
+      room._count.emergency_room_beds
+    ),
+  }));
   return (
     <div
       key={index}
@@ -28,13 +48,14 @@ export default function EmergencyBox({
           "REGIONAL_EMERGENCY_MEDICAL_CENTER"
             ? "지역응급의료센터"
             : emergencyCenter.emergency_center_type ==
-              "LOCAL_EMERGENCY_MEDICAL_INSTITUTION"
-            ? "지역응급의료기관"
-            : "권역응급의료센터"
+                "LOCAL_EMERGENCY_MEDICAL_INSTITUTION"
+              ? "지역응급의료기관"
+              : "권역응급의료센터"
         }`}
         distance={(Number(emergencyCenter.distance) / 1000).toFixed(1)}
         number={emergencyCenter.emergency_center_primary_phone}
         location={emergencyCenter.emergency_center_address}
+        roomInfo={roomInfo}
       />
     </div>
   );
