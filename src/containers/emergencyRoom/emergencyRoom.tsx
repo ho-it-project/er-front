1fbdbea8451ae497d8e85fd6319e17cc35f4e3e8
ejinn1 <ejinn1@naver.com>
2023-12-09 23:53:30 +0900

    fix: 메인 페이지에서 환자 있는 병상 누르면 환자 상세정보모달창 뜨게 수정

--- src/containers/emergencyRoom/emergencyRoom.tsx
+++ src/containers/emergencyRoom/emergencyRoom.tsx
@@ -1,5 +1,9 @@
+import useModal from "@/hooks/useModal";
 import { emergencyRoomBed } from "@/states/EmergencyCenterInfoStore";
-import React from "react";
+import { PatientSummary } from "@/states/patientsStore";
+import useUserStore from "@/states/userStore";
+import React, { useState } from "react";
+import PatientDetailModal from "../patients/patientDetailModal";
 import Bed from "./Bed";
 
 interface EmergencyRoomProps {
@@ -7,17 +11,48 @@ interface EmergencyRoomProps {
 }
 
 const EmergencyRoom = React.memo(({ beds }: EmergencyRoomProps) => {
+  const { accessToken } = useUserStore();
+  const { isOpen, openModal, closeModal } = useModal();
+  const [selected, setSelected] = useState<PatientSummary>();
+  const clickedPatient = (id: string | null) => {
+    if (id !== null) {
+      const url = `/api/er/patients/${id}`;
+      const options = {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((data) => {
+          setSelected(data.result);
+          openModal();
+        })
+        .catch((error) => console.error(error));
+    }
+  };
+
   return (
-    <div className="mt-4 grid grid-cols-[repeat(auto-fill,minmax(24rem,24rem))] gap-[2rem]">
-      {beds &&
-        beds.map((bed) => (
-          <Bed
-            key={bed.emergency_room_bed_num}
-            bed={bed}
-            patientId={bed.patient_id}
-          />
-        ))}
-    </div>
+    <>
+      <div className="mt-4 grid grid-cols-[repeat(auto-fill,minmax(24rem,24rem))] gap-[2rem]">
+        {beds &&
+          beds.map((bed) => (
+            <div
+              key={bed.emergency_room_bed_num}
+              className="cursor-pointer"
+              onClick={() => clickedPatient(bed.patient_id)}
+            >
+              <Bed bed={bed} patientId={bed.patient_id} />
+            </div>
+          ))}
+      </div>
+      {isOpen && selected && (
+        <PatientDetailModal patient={selected} close={closeModal} />
+      )}
+    </>
   );
 });
 
